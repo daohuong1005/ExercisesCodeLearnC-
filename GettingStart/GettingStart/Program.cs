afb552292a8f0bf8df29ce3ddbbfdeabf6cdf72a
DaoXuanHuong <DaoXuanHuong@DESKTOP-Q8K3OFI>
2021-02-01 10:55:09 +0700

    - Know how to create a new project named GettingStart in Visual Studio. -Allow user enter his/her name.

---
+++ GettingStart/GettingStart/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GettingStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User enter his/her name : ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Your name is : " + name);
+
+            Console.ReadKey();
+        }
+    }
+}
